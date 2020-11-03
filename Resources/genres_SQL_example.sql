995e8c4320b27f8d44542c5c7c0f370d987eda03
Matthew Tichenor <66046978+MatthewTichenor@users.noreply.github.com>
2020-11-02 21:10:06 -0600

    Update genres_SQL_example.sql

--- Resources/genres_SQL_example.sql
+++ Resources/genres_SQL_example.sql
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 select m.id, m.title, m.status, m.release_date
 from movies_metadata m left join
 genres g on g.id = m.id
-=======
-select m.id, m.title, m.status, m.release_date
-from movies_metadata m left join
-genres g on g.id = m.id
->>>>>>> main
 where g.Fantasy is True
