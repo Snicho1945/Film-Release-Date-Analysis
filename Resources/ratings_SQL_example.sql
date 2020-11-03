436dada64de0bc22f1e86acf3ef1a005eda6a19c
Matthew Tichenor <66046978+MatthewTichenor@users.noreply.github.com>
2020-11-02 21:05:13 -0600

    Update ratings_SQL_example.sql

--- Resources/ratings_SQL_example.sql
+++ Resources/ratings_SQL_example.sql
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 select m.id, m.title, m.company, r.rating
 from movies_metadata m left join
 ratings r on r.id= m.id
-=======
-select m.id, m.title, m.company, r.rating
-from movies_metadata m left join
-ratings r on r.id= m.id
->>>>>>> main
 where m.title ='Jumanji'
