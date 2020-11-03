1df33e70de6f0a54db8ad5af4a6b95e82cd03d8c
Matthew Tichenor <66046978+MatthewTichenor@users.noreply.github.com>
2020-11-02 21:11:19 -0600

    Update keywords_SQL_example.sql

--- Resources/keywords_SQL_example.sql
+++ Resources/keywords_SQL_example.sql
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 select m.id, m.title, m.company, k.keywords
 from movies_metadata m left join
 keywords k on k.id= m.id
-=======
-select m.id, m.title, m.company, k.keywords
-from movies_metadata m left join
-keywords k on k.id= m.id
->>>>>>> main
 where m.title ='GoldenEye'
